da84298f9b0758ab1e5d90a76fc00e5599dd0f1e
Angus Liu <angusliu1975@gmail.com>
2022-03-13 10:51:51 +0000

    [add] build script: init laravel db

---
+++ _build/conf/local.php
@@ -0,0 +1,7 @@
+<?php
+$cfg = array();
+$cfg['DB_HOST'] = 'localhost';
+$cfg['DB_PORT'] = 3306;
+$cfg['DB_DATABASE'] = 'lara_app';
+$cfg['DB_USERNAME'] = 'angus';
+$cfg['DB_PASSWORD'] = '';
---
+++ _build/init_laravel_db.php
@@ -0,0 +1,54 @@
+#!/usr/bin/env php
+<?php
+define('LARAVEL_ENV_FILE', '../.env');
+
+$env = isset($argv[1])? $argv[1] // set first argument as environment
+    : 'local'; // or use default environment
+$cfg_file = "conf/$env.php";
+echo 
+<<<EOD
+Environment: $env
+Configuration: $cfg_file\n
+EOD;
+
+include_once($cfg_file);
+if ( !is_array($cfg) ) die ("Cannot find '$env' configuration object in '{$cfg_file}'!\n");
+
+$cmd_lines = <<<EOD
+sudo mysql -u root -e 'CREATE USER IF NOT EXISTS `{$cfg['DB_USERNAME']}`@`{$cfg['DB_HOST']}` IDENTIFIED BY "{$cfg['DB_PASSWORD']}";'
+sudo mysql -u root -e 'CREATE DATABASE IF NOT EXISTS `{$cfg['DB_DATABASE']}`;'
+sudo mysql -u root -e 'GRANT ALL PRIVILEGES ON `{$cfg['DB_DATABASE']}`.* TO `{$cfg['DB_USERNAME']}`@`{$cfg['DB_HOST']}`;'
+sudo mysql -u root -e 'FLUSH PRIVILEGES;'
+EOD;
+
+/*
+SELECT host, user, authentication_string FROM mysql.user // for mysql verion > 5.7
+//*/
+
+$cmds = explode("\n", $cmd_lines);
+foreach($cmds as $cmd) {
+    echo "Command: $cmd\n";
+    system($cmd, $ret);
+    echo "Result: $ret\n";
+
+    if ( $ret ) break;
+}
+
+// read and backup .env
+$env_contents = file_get_contents(LARAVEL_ENV_FILE);
+$env_backup_file = "./" . hrtime(true) . ".env.bak";
+
+// set .env accordingly
+echo
+<<<EOD
+Rewrite Laravel '.env' for DB settings...\n
+EOD;
+
+file_put_contents($env_backup_file, $env_contents);
+foreach($cfg as $key => $val) {
+    $env_contents = preg_replace("/^(\s*$key\s*=)(.*)$/m", "$key=$val", $env_contents); // set modifier to multiline for using ^$
+}
+file_put_contents(LARAVEL_ENV_FILE, $env_contents);
+
+
+
