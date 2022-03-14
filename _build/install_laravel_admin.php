2ff61e1820820d8f98d50fed69ba52f319019a4e
Angus Liu <angusliu1975@gmail.com>
2022-03-14 03:52:12 +0000

    [add] build script: install extension encore/laravel-admin

---
+++ _build/install_laravel_admin.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+$cmd_lines = <<<EOD
+cd ../ && composer require encore/laravel-admin
+cd ../ && php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
+cd ../ && php artisan admin:install
+EOD;
+
+$cmds = explode("\n", $cmd_lines);
+foreach($cmds as $cmd) {
+    echo "Command: $cmd\n";
+    system($cmd, $ret);
+    echo "Result: $ret\n";
+
+    if ( $ret ) break;
+}
