953c09bce8fe2713b132655b53dccde9c51666fc
Angus Liu <angusliu1975@gmail.com>
2022-03-14 04:06:32 +0000

    [add] build script: new APP settings in 'local' config

--- _build/conf/local.php
+++ _build/conf/local.php
@@ -1,5 +1,11 @@
 <?php
 $cfg = array();
+
+$cfg['APP_NAME'] = 'Lara-App';
+$cfg['APP_ENV'] = 'local';
+$cfg['APP_DEBUG'] = 'true';
+$cfg['APP_URL'] = 'http://192.168.1.88:9999';
+
 $cfg['DB_HOST'] = 'localhost';
 $cfg['DB_PORT'] = 3306;
 $cfg['DB_DATABASE'] = 'lara_app';
