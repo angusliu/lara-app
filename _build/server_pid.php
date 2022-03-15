91e635573361576b67d71c57f5348e94f580ed08
Angus Liu <angusliu1975@gmail.com>
2022-03-15 12:54:37 +0000

    [improve] build script: get correct pid with matched port number

--- _build/server_pid.php
+++ _build/server_pid.php
@@ -1,14 +1,28 @@
 #!/usr/bin/env php
 <?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+
 define('LARAVEL_ROOT', realpath(__DIR__ . '/../'));
 
+$server_port = 8000; // default port
 $server_pid_file = __DIR__ . '/server.pid';
 $server_php_file = LARAVEL_ROOT . '/server.php';
+
+// load LARAVEL_ROOT/.env as an array
+$env = Dotenv\Dotenv::createArrayBacked(LARAVEL_ROOT)->load();
+if ( !is_array($env) )
+    fwrite(STDERR, "Cannot find environment object in folder '" . LARAVEL_ROOT . "'!\n") && exit(-1);
+
+// read server port
+$app_url = isset($env['APP_URL'])?trim($env['APP_URL']):'';
+if ( $app_url && preg_match('/.*:(\d+)/', $app_url, $matches) ) {
+    $server_port = intval($matches[1]);
+}
 
 // use `ps ax` to show all processes (a: all of tty processes, ax: all of tty and non-tty processes)
 $cmd =
 <<<EOD
-ps ax | grep $server_php_file | grep -v grep
+ps ax | grep ':$server_port $server_php_file' | grep -v grep
 EOD;
 
 // calculate timeout ourselves, because set_time_limit() or ini_set('max_execution_time', $sec)
