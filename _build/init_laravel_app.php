180d031b12312c3e098af3bfc8cbaedc8fc44ba1
Angus Liu <angusliu1975@gmail.com>
2022-03-16 06:50:24 +0000

    [fix] build script: relative path problem

--- _build/init_laravel_app.php
+++ _build/init_laravel_app.php
@@ -1,13 +1,16 @@
 #!/usr/bin/env php
 <?php
+define('LARAVEL_ROOT', realpath(__DIR__ . '/../'));
+
 /* `composer create-project` is the same as:
     - cp .env.example .env
     - composer install
     - php artisan key:generate --ansi
-//*/ 
+//*/
+$laravel_root = LARAVEL_ROOT;
 $cmd =
 <<<EOD
-cd ../ && composer create-project
+cd $laravel_root && composer create-project
 EOD;
 
 echo "Command: $cmd\n";
