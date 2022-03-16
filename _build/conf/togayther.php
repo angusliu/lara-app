3a63ac7565e65b662e882c072e6ad5fbb447dd88
Angus Liu <angusliu1975@gmail.com>
2022-03-16 06:17:57 +0000

    [add] build script: new togayther configuration

---
+++ _build/conf/togayther.php
@@ -0,0 +1,15 @@
+<?php
+$cfg = array();
+
+$cfg['APP_NAME'] = 'Togayther';
+$cfg['APP_ENV'] = 'local';
+$cfg['APP_DEBUG'] = 'true';
+$cfg['APP_URL'] = 'http://192.168.1.88:8888';
+
+$cfg['ADMIN_HTTPS'] = 'false';
+
+$cfg['DB_HOST'] = 'localhost';
+$cfg['DB_PORT'] = 3306;
+$cfg['DB_DATABASE'] = 'togayther';
+$cfg['DB_USERNAME'] = 'angus';
+$cfg['DB_PASSWORD'] = '';
