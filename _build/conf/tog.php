e2ed49ec9dc1fc26820a0d5bb65201da0f4cf629
Angus Liu <angusliu1975@gmail.com>
2022-03-17 14:17:03 +0000

    [add] build script: new configuration

---
+++ _build/conf/tog.php
@@ -0,0 +1,15 @@
+<?php
+$cfg = array();
+
+$cfg['APP_NAME'] = 'Togayther';
+$cfg['APP_ENV'] = 'local';
+$cfg['APP_DEBUG'] = 'true';
+$cfg['APP_URL'] = 'http://tog.angusliu.com:80';
+
+$cfg['ADMIN_HTTPS'] = 'false';
+
+$cfg['DB_HOST'] = 'localhost';
+$cfg['DB_PORT'] = 3306;
+$cfg['DB_DATABASE'] = 'togayther';
+$cfg['DB_USERNAME'] = 'angus';
+$cfg['DB_PASSWORD'] = '';
