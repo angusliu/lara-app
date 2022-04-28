5217663993dbf1b10b900728346676b039156b1c
Angus Liu <angusliu1975@gmail.com>
2022-04-28 06:37:51 +0000

    [add] socialiteproviders/zoho

--- _build/install_pkg_socialite.php
+++ _build/install_pkg_socialite.php
@@ -4,6 +4,7 @@
 packages:
 https://github.com/laravel/socialite
 official:
+https://laravel.com/docs/9.x/socialite
 https://socialiteproviders.com/
 docs:
 https://laravel.com/docs/9.x/socialite#introduction
@@ -11,6 +12,7 @@ https://laravel.com/docs/9.x/socialite#introduction
 //*/
 $cmd_lines = <<<EOD
 cd ../ && composer require laravel/socialite
+cd ../ && composer require socialiteproviders/zoho
 EOD;
 
 $cmds = explode("\n", $cmd_lines);
