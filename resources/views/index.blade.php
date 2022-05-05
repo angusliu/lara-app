0fc5567a5f01447b6ec77c1ceec1fb4b0464086b
Angus Liu <angusliu1975@gmail.com>
2022-05-05 13:46:20 +0000

    [update] index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,30 +9,45 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
-    Hello, World!
 
+    <div class="container-fluid">
+
+    <div class="row justify-content-center my-1">
+        <div class="col-8 col-sm-6 col-lg-4">
+        Hello, World!
+        </div>
+    </div>
+    
     {{-- Login with Facebook --}}
-    <div class="flex items-center justify-end mt-4">
-        <a class="btn" href="{{ url('login/facebook') }}"
-            style="background: #3B5499; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
-            Facebook 登入
-        </a>
+    <div class="row justify-content-center my-1">
+        <div class="col-8 col-sm-6 col-lg-4">
+            <a class="btn" href="{{ url('login/facebook') }}"
+                style="background: #3B5499; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
+                Facebook 登入
+            </a>
+        </div>
     </div>
 
     {{-- Login with Google account --}}
-    <div class="flex items-center justify-end mt-4">
-        <a class="btn" href="{{ url('login/google') }}"
-            style="background: #FF3333; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
-            Google 帳戶登入
-        </a>
+    <div class="row justify-content-center my-1">
+        <div class="col-8 col-sm-6 col-lg-4">
+            <a class="btn" href="{{ url('login/google') }}"
+                style="background: #FF3333; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
+                Google 帳戶登入
+            </a>
+        </div>
     </div>
 
     {{-- Login with LINE account --}}
-    <div class="flex items-center justify-end mt-4">
-        <a class="btn" href="{{ url('login/line') }}"
-            style="background: #33AA33; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
-            LINE 帳號登入
-        </a>
+    <div class="row justify-content-center my-1">
+        <div class="col-8 col-sm-6 col-lg-4">
+            <a class="btn" href="{{ url('login/line') }}"
+                style="background: #33AA33; color: WHITE; padding: 5px; width: 100%; text-align: center; border-radius:5px;  display: block;">
+                LINE 帳號登入
+            </a>
+        </div>
+    </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
